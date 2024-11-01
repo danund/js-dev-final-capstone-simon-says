924692507f15538765ee26571bc6acfebfc771f3
danund <167810826+danund@users.noreply.github.com>
2024-11-01 18:35:44 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -23,22 +23,22 @@ const pads = [
   {
     color: "red",
     selector: document.querySelector(".js-pad-red"),
-    sound: new Audio("../assets/simon-says-sound-1.mp3"),
+    sound: new Audio("https://github.com/danund/js-dev-final-capstone-simon-says/tree/main/assets/simon-says-sound-1.mp3?raw=true"),
   },
   {
     color: "green",
     selector: document.querySelector(".js-pad-green"),
-    sound: new Audio("../assets/simon-says-sound-2.mp3"),
+    sound: new Audio("https://github.com/danund/js-dev-final-capstone-simon-says/tree/main/assets/simon-says-sound-2.mp3?raw=true"),
   },
   {
     color: "blue",
     selector: document.querySelector(".js-pad-blue"),
-    sound: new Audio("../assets/simon-says-sound-3.mp3"),
+    sound: new Audio("https://github.com/danund/js-dev-final-capstone-simon-says/tree/main/assets/simon-says-sound-3.mp3?raw=true"),
   },
   {
     color: "yellow",
     selector: document.querySelector(".js-pad-yellow"),
-    sound: new Audio("../assets/simon-says-sound-4.mp3"),
+    sound: new Audio("https://github.com/danund/js-dev-final-capstone-simon-says/tree/main/assets/simon-says-sound-4.mp3?raw=true"),
   },
 ];
 
